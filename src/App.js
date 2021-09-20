97642f7f6d18b21884b4cc8d73102a2220a57ed5
JamesFE <cristina@jamesfe.lan>
2021-09-20 09:32:21 -0400

    Grid Layout

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Body</h1>
-      
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-2"><h1>Types</h1></div>
+          <div class="col-sm-10"><center><h1>Body</h1></center></div>
+        </div>
+      </div>
     </div>
   );
 }
