e4576f07e61edb0a0e49667dd8da5538d2703b55
JamesFE <cristina@jamesfe.lan>
2021-09-20 09:10:58 -0400

    Bootstrap v5.1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="App">
       <h1>JOSN PLACEHOLDER</h1>
+      <button type="button" class="btn btn-primary btn-lg">Large button</button>
     </div>
   );
 }
