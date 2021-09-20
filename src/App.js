ceb71db7c15f6222ed06294a331dbd9e7e191af1
JamesFE <cristina@jamesfe.lan>
2021-09-20 10:15:59 -0400

    Axios HTTP & State

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
+import React, { Component } from "react"
+import axios from "axios"
 import Header from "./components/Header"
+import Home from "./components/Home"
 
 
-function App() {
+class App extends Component {
+
+  state = { user : null  }
+
+  async componentDidMount() {
+    let result = await axios.get("https://jsonplaceholder.typicode.com/users/3")
+    console.log(result)
+    this.setState({ user: result.data });
+}
+
+  render() {
   return (
     <div className="App">
       <Header />
-      <div class="container">
         <div class="row">
-          <div class="col-sm-2"><h1>Types</h1></div>
-          <div class="col-sm-10"><center><h1>Body</h1></center></div>
+          <div class="col-sm-2">
+            <h1>Posts</h1>
+            <h1>Photos</h1>
+            <h1>Albums</h1>
+            <h1>Todos</h1>
+          </div>
+          <div class="col-sm-10">
+            <div class="container">
+              <Home />
+            </div>
+          </div>
         </div>
-      </div>
     </div>
   );
+  }
 }
 
 export default App;
